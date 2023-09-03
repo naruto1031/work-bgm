7fe8df19162f8132193b5dd258699d4e7ec0b7ef
naruto1031 <naruto0614@au.com>
2023-09-03 16:03:30 +0900

    選択して動画を表示する機能の実装

--- src/app/components/Player.tsx
+++ src/app/components/Player.tsx
@@ -2,21 +2,48 @@
 import { FC, useEffect, useState } from 'react';
 import ReactPlayer from 'react-player'
 import { css } from '../../../styled-system/css';
+import { hstack } from '../../../styled-system/patterns';
+
+type Data = {
+	title: string,
+	url: string[]
+}
 
 type PlayerProps = {
 	url: string;
+	data: Data[]
 }
 
-export const Player: FC<PlayerProps> = ({url}) => {
+export const Player: FC<PlayerProps> = ({ url, data }) => {
 	const [hasWindow, setHasWindow] = useState(false)
+	const [currentTitle, setCurrentTitle] = useState("")
 	useEffect(() => {
-    if (typeof window !== "undefined") {
-      setHasWindow(true);
-    }
-  }, []);
+		if (typeof window !== "undefined") {
+			setHasWindow(true);
+		}
+	}, []);
+	const handleClick = (title: string) => {
+    setCurrentTitle(title)
+  }
 	return (
-    <div className={css({borderRadius: "10px", w: "fit-content"})}>
-      {hasWindow && <ReactPlayer url={url} width={"100%"}/>}
-    </div>
+		<>
+			<div className={hstack({justifyContent: "center"})}>
+				{data.map((item, i) => (
+					<div key={i} onClick={()=>handleClick(item.title)}>{item.title}</div>
+				))}
+			</div>
+			{data.map((item, i)=>(
+			 <div key={i} className={css({maxWidth: "90vw", margin: "0 auto"})}>
+					{item.title === currentTitle && 
+						item.url.map((url, j)=>(
+							<div key={j} className={css({margin: "20px"})}>
+								{hasWindow && <ReactPlayer url={url} width={"100%"} />}
+							</div>
+						))
+					}
+				</div>	
+			))}
+		</>
+
 	)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,45 @@
+import { url } from "inspector";
 import { hstack, vstack } from "../../styled-system/patterns";
+import { css } from "../../styled-system/css";
 import { Player } from "./components/Player";
 
 export default function Page() {
+  const data = [
+    {
+      title: "Lofi",
+      url: [
+        "https://www.youtube.com/watch?v=jfKfPfyJRdk"
+      ]
+    }, 
+    {
+      title: "ジャズ",
+      url: ["https://www.youtube.com/watch?v=LnJz8iVo6CA","https://www.youtube.com/watch?v=2VrwGKAYnpM", "https://www.youtube.com/watch?v=wH-TyUFgdMs"]
+    },
+    {
+      title: "雨の日",
+      url: [
+       "https://www.youtube.com/watch?v=5Q2Pc-e-8Qc&t=1242s"
+      ]
+    },
+    {
+      title: "夏の日",
+      url: [
+        "https://www.youtube.com/watch?v=5Q2Pc-e-8Qc&t=1242s"
+      ]
+    },
+    {
+      title: "カフェ",
+      url: [
+        "https://www.youtube.com/watch?v=OLl8ZgqCZyo"
+      ]
+    }
+  ]
+
   return (
-    <main className={hstack({alignItems: "center", w: "100%",  h: "100vh", justifyContent: "center", backgroundColor: "gray.300"})}>
-      <Player url="https://youtube.com/playlist?list=PLXrD4cv5Ic-w_W9vpv43-Y8oZWgbiZwPt&si=cfcWepiMbLjlC6IV" />
+    <main className={css({ margin: "0 auto", textAlign: "center", w: "100vw", h:"100vh", backgroundColor: "gray.300", overflow: "auto"})}>
+      <h1 className={css({fontSize: "2xl"})}>作業用BGMアプリ</h1>
+      <p>今日はどんな音楽が聴きたい日？</p>
+      <Player url="https://youtube.com/playlist?list=PLXrD4cv5Ic-w_W9vpv43-Y8oZWgbiZwPt&si=cfcWepiMbLjlC6IV" data={data} />
     </main>
   );
 }
